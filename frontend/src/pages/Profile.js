a921cd3d7c6e04f73dba942a3b46fcd30b1398d3
Yassine Sallemi <yessine.sallemi.9@gmail.com>
2024-03-23 17:34:28 +0100

    profile page added

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="dark:bg-palenight min-h-screen w-screen">
+      <h1>Profile Page</h1>
+      {/* Add your profile content here */}
+    </div>
+  );
+};
+
+export default Profile;

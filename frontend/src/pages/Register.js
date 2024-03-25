c54d8c5769b36e96653265d2bd0afc648b1e8a7c
Yassine Sallemi <yessine.sallemi.9@gmail.com>
2024-03-25 17:51:34 +0100

    update: minor changes

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import trollFace from "../images/troll_face.png";
-import Capture from '../pages/images/Capture.PNG';
+import Capture from "../images/Capture.PNG";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -21,113 +21,121 @@ const Register = () => {
   };
 
   return (
-      <section className="dark:bg-palenight min-h-screen w-screen">
-        <div className="min-h-screen ">
-          <div className="flex flex-col items-center justify-center px-6 py-3 mx-auto md:h-screen lg:py-0">
-            <Link   to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"  >
-                <img className="w-8 h-8 mr-2" src={trollFace} alt="Troll Face" />
-                MemeHub
-            </Link>
-            <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-gray-800 rounded-xl mx-auto shadow-lg overflow-hidden">
-              <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center">
-                <img className="w-full h-full" src={Capture} alt="Spongebob" />
-              </div>
-              <div className="w-full lg:w-1/2 py-16 px-12">
-                <h2  className="text-3xl text-gray-300 mb-4">Register</h2>
-                <p className="mb-4 text-gray-300">
-                  Create your account. It’s free and only take a minute
-                </p>
-                <form
-                      className="space-y-4 md:space-y-6"
-                      action="#"
-                      onSubmit={handleSubmit}
-                    >
-                  <div>
-                    <label
-                      for="username"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Username
-                    </label>
-                    <input
-                      type="text"
-                      name="username"
-                      id="username"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Username"
-                      required=""
-                      value={username}
-                      onChange={handleUsernameChange}
-                    />
-                  </div>
-                  <div>
-                    <label
-                      for="email"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Email address"
-                      required=""
-                    />
-                  </div>
-                  <div>
-                    <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      required=""
-                      value={password}
-                      onChange={handlePasswordChange}
-                    /> 
-                  </div>
-                  <div>
-                    <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Confirm password
-                    </label>
-                    <input
-                      type="password"
-                      name="confirm-password"
-                      id="confirm-password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      required=""
-                    /> 
-                  </div>
-                  <div className="mt-5 flex">
-                    <input type="checkbox" className="border border-gray-400" required/>
-                    <span className="text-gray-500 ml-3">
-                        I accept the terms of use and privacy policy
-                    </span>
-                  </div>
-                      <button
-                        type="submit"
-                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                      >Register Now</button>
-                </form>
-              </div>
+    <section className="dark:bg-palenight min-h-screen w-screen">
+      <div className="min-h-screen ">
+        <div className="flex flex-col items-center justify-center px-6 py-3 mx-auto md:h-screen lg:py-0">
+          <Link
+            to="/"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img className="w-8 h-8 mr-2" src={trollFace} alt="Troll Face" />
+            MemeHub
+          </Link>
+          <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-gray-800 rounded-xl mx-auto shadow-lg overflow-hidden">
+            <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center">
+              <img className="w-full h-full" src={Capture} alt="Spongebob" />
+            </div>
+            <div className="w-full lg:w-1/2 py-16 px-12">
+              <h2 className="text-3xl text-gray-300 mb-4">Register</h2>
+              <p className="mb-4 text-gray-300">
+                Create your account. It’s free and only take a minute
+              </p>
+              <form
+                className="space-y-4 md:space-y-6"
+                action="#"
+                onSubmit={handleSubmit}
+              >
+                <div>
+                  <label
+                    for="username"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Username"
+                    required=""
+                    value={username}
+                    onChange={handleUsernameChange}
+                  />
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Email address"
+                    required=""
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                </div>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    type="password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required=""
+                  />
+                </div>
+                <div className="mt-5 flex">
+                  <input
+                    type="checkbox"
+                    className="border border-gray-400"
+                    required
+                  />
+                  <span className="text-gray-500 ml-3">
+                    I accept the terms of use and privacy policy
+                  </span>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Register Now
+                </button>
+              </form>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
 export default Register;
-

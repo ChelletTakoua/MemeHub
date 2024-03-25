18f16e5a0dbd3fd6340e50d4d3a5c4990a340233
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-03-25 05:40:20 +0100

    added notes and TODOs
    explained how to work with the project

--- backend/src/Controllers/ExampleController.php
+++ backend/src/Controllers/ExampleController.php
@@ -9,10 +9,33 @@ use Exceptions\HttpExceptions\UnauthorizedException;
 class ExampleController
 {
 
-    /**
-     * @throws HttpException
-     */
-    public function example($exampleParam)
+public function referenceMethod()
+    {
+        header('Content-Type: application/json'); // if you want to return json
+
+        // your code logic goes here
+
+
+        $exempleObjectToReturn = [
+            "id" => 1,
+            "title" => "Sample Post",
+            "content" => "This is a sample post content."
+        ];
+
+        echo json_encode($exempleObjectToReturn); // to send the response
+
+        // any error thrown and not caught will be handled by the error handler
+        // HttpException is a custom exception that can be thrown to return a specific HTTP status code
+        // you don't need to manually set the status code, it will be done by the error handler
+        // exemple:
+        //      throw new HttpException('Not Found', 404);
+        //  or a more specific exception:
+        //      throw new UnauthorizedException();
+        // you can create your own exceptions by extending the HttpException class and setting the status code (and message) in the constructor
+
+    }
+
+    public function example2($exampleParam)
     {
         header('Content-Type: application/json');
 

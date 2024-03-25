dce03793d0b677491f956bd572ead6457f13caa1
loueysioua <louey.sioua@insat.ucar.tn>
2024-03-25 07:53:49 +0100

    fixed some styling bugs

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -21,31 +21,31 @@ const Register = () => {
   };
 
   return (
-      <section class="dark:bg-palenight min-h-screen w-screen">
-        <div class="min-h-screen ">
-          <div class="flex flex-col items-center justify-center px-6 py-3 mx-auto md:h-screen lg:py-0">
-            <Link   to="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"  >
-                <img class="w-8 h-8 mr-2" img src={trollFace} alt="Troll Face" />
+      <section className="dark:bg-palenight min-h-screen w-screen">
+        <div className="min-h-screen ">
+          <div className="flex flex-col items-center justify-center px-6 py-3 mx-auto md:h-screen lg:py-0">
+            <Link   to="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"  >
+                <img className="w-8 h-8 mr-2" src={trollFace} alt="Troll Face" />
                 MemeHub
             </Link>
-            <div class="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
-              <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center">
+            <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-gray-800 rounded-xl mx-auto shadow-lg overflow-hidden">
+              <div className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center">
                 <img className="w-full h-full" src={Capture} alt="Spongebob" />
               </div>
-              <div class="w-full lg:w-1/2 py-16 px-12">
-                <h2  class="text-3xl mb-4">Register</h2>
-                <p class="mb-4">
+              <div className="w-full lg:w-1/2 py-16 px-12">
+                <h2  className="text-3xl text-gray-300 mb-4">Register</h2>
+                <p className="mb-4 text-gray-300">
                   Create your account. It’s free and only take a minute
                 </p>
                 <form
-                      class="space-y-4 md:space-y-6"
+                      className="space-y-4 md:space-y-6"
                       action="#"
                       onSubmit={handleSubmit}
                     >
                   <div>
                     <label
                       for="username"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Username
                     </label>
@@ -53,7 +53,7 @@ const Register = () => {
                       type="text"
                       name="username"
                       id="username"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Username"
                       required=""
                       value={username}
@@ -63,7 +63,7 @@ const Register = () => {
                   <div>
                     <label
                       for="email"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Email
                     </label>
@@ -71,7 +71,7 @@ const Register = () => {
                       type="email"
                       name="email"
                       id="email"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Email address"
                       required=""
                     />
@@ -79,7 +79,7 @@ const Register = () => {
                   <div>
                     <label
                       for="password"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Password
                     </label>
@@ -88,7 +88,7 @@ const Register = () => {
                       name="password"
                       id="password"
                       placeholder="••••••••"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       required=""
                       value={password}
                       onChange={handlePasswordChange}
@@ -97,7 +97,7 @@ const Register = () => {
                   <div>
                     <label
                       for="password"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Confirm password
                     </label>
@@ -106,19 +106,19 @@ const Register = () => {
                       name="confirm-password"
                       id="confirm-password"
                       placeholder="••••••••"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       required=""
                     /> 
                   </div>
-                  <div class="mt-5">
-                    <input type="checkbox" class="border border-gray-400" required/>
-                    <span>
+                  <div className="mt-5 flex">
+                    <input type="checkbox" className="border border-gray-400" required/>
+                    <span className="text-gray-500 ml-3">
                         I accept the terms of use and privacy policy
                     </span>
                   </div>
                       <button
                         type="submit"
-                        class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                       >Register Now</button>
                 </form>
               </div>

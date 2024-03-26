f6c54018e649c7dfbd9b5f77e2895cd0ce50e035
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-03-26 08:03:17 +0100

    Update errorHandler.php
    When there is an internalServerException, the Error handler logs the error and you can click the link to go to the line of code that caused the error

--- backend/src/Exceptions/errorHandler.php
+++ backend/src/Exceptions/errorHandler.php
@@ -5,14 +5,16 @@ function logError($e)
 {
     // Log the error message, file, line number, class, method, and stack trace
     $logMessage = sprintf(
-        "\033[31mError: %s in %s on line %d\n%s\n%s::%s\nStack trace:\n%s\033[0m",
+        "\033[31mError: %s in %s on line %d\n%s\n%s::%s\nStack trace:\n%s\n\nLink to code: vscode://file/%s:%d\033[0m",
         $e->getMessage(),
         $e->getFile(),
         $e->getLine(),
         $e->getMessage(),
         $e->getFile(),
         $e->getLine(),
-        $e->getTraceAsString()
+        $e->getTraceAsString(),
+        $e->getFile(),
+        $e->getLine()
     );
     error_log($logMessage);
 }

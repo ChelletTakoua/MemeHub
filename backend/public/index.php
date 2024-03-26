99f48a2b7c3cc10a908b79f30330bc1bfca269d2
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-03-26 02:57:29 +0100

    updated errorHandler
    now the error handler logs the errors for debugging

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,18 +2,21 @@
 session_start();
 
 require_once '../src/autoload.php';
-
 require_once '../src/Exceptions/ErrorHandler.php';
-//set_exception_handler('errorHandler');
+set_exception_handler('errorHandler');
+
 
 
 
 $router = new Router\Router($_SERVER['REQUEST_URI']);
 require '../src/Router/routes.php';
 
+
+
 $router->run();
 $connection = \Database\DatabaseConnection::getInstance();
 echo"\n\n";
+
 var_dump($connection);
 echo __DIR__ . "/../src/config/database.php";
 
--- backend/src/Exceptions/errorHandler.php
+++ backend/src/Exceptions/errorHandler.php
@@ -1,8 +1,26 @@
 <?php
 
 
+function logError($e)
+{
+    // Log the error message, file, line number, class, method, and stack trace
+    $logMessage = sprintf(
+        "\033[31mError: %s in %s on line %d\n%s\n%s::%s\nStack trace:\n%s\033[0m",
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine(),
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine(),
+        $e->getTraceAsString()
+    );
+    error_log($logMessage);
+}
+
 function errorHandler($e)
 {
+    logError($e);
+
     $response = [
         'success' => false,
         'message' => ''

6ac44eb185dded0d2032dd0127eaafee4d2f71f3
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-03-27 21:26:11 +0100

    Update TestController.php

--- backend/src/Controllers/TestController.php
+++ backend/src/Controllers/TestController.php
@@ -11,15 +11,11 @@ class TestController
 {
 
     //use this method if you want to test some code, call it with route /test
-    //TODO: add to gitignore after next commit
     public function testMethod()
     {
-        /*$userTableManager = UserTableManager::getInstance();
-        var_dump($userTableManager);*/
 
-        $user = new User(1,2,3,4,5,6);
-        $user->save();
-
+        // test anything you want here
+        // this file is in the gitignore, anything you write here will not be pushed to the repository
 
     }
 

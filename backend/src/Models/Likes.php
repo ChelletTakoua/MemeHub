0a65a3711819ba5d5deeb2f4d3a9dbd59a0c7f2d
fouziMariem <mariem.elfouzi@insat.ucar.tn>
2024-03-28 01:32:41 +0100

    added likes

---
+++ backend/src/Models/Likes.php
@@ -0,0 +1,25 @@
+<?php
+
+class Likes {
+    private $id;
+    private $memeId;
+    private $userId;
+
+    public function __construct($id, $memeId, $userId) {
+        $this->id = $id;
+        $this->memeId = $memeId;
+        $this->userId = $userId;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getMemeId() {
+        return $this->memeId;
+    }
+
+    public function getUserId() {
+        return $this->userId;
+    }
+}

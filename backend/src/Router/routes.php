7f12edfbc8e407bbc07bc6ba7116684e0e859931
medtaher <56443200+medtaher123@users.noreply.github.com>
2024-03-29 22:45:52 +0100

    Update routes.php

--- backend/src/Router/routes.php
+++ backend/src/Router/routes.php
@@ -2,17 +2,19 @@
 // all your routes will go here
 
 global $router;
-    
+
 
 // use this route to test your code (len t7eb ttesti ayy haja)  Yaa Sioua meghir ma tbaddel fl index.php 5allih rayedh!!
 $router->get('/test', 'TestController@testMethod',['guest']);
-$router->get('/test', 'TestController@testMethod',['guest']);
+$router->post('/test', 'TestController@testMethodPost',['guest']);
 
 
-$router->get('/', function (){echo "welcome to the homepage";});
+
+$router->get('/', function (){echo "welcome to the homepage GET";},['guest']);
+$router->post('/', function (){echo "welcome to the homepage POST";},['guest']);
 
 $router->get('/users', "UserController@getAllUsers", ['user', 'admin']);
-$router->get('/users/:id', "UserController@getUserById", ['admin']);
+$router->get('/users/:id', "UserController@getUserById", ['user']);
 $router->post('/users/:id', "UserController@getUserById", ['admin']);
 $router->get('/example', "ExampleController@referenceMethod", ['user', 'admin']);
 

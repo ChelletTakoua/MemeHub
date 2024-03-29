3d1f5a332823ae3cca5df73fd68b364f6939fa89
unknown <taki74ayadi@gmail.com>
2024-03-29 23:46:44 +0100

    Mail class

--- backend/src/Utils/Mail.php
+++ backend/src/Utils/Mail.php
@@ -1,1 +1,33 @@
 <?php
+namespace Utils;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+class Mail{
+    
+    public function sendMail($to,$subject,$message){
+    $mail=new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host='smtp.gmail.com';
+    $mail->SMTPAuth=true;
+    $mail->Username='chellettakoua@gmail.com';
+    $mail->Password='wqvbhyxgjvrhwkzp';
+    $mail->SMTPSecure='tls';
+    $mail->Port=587;
+    $mail->setFrom('chellettakoua@gmail.com');
+
+    $mail->addAddress($to);
+    $mail->Subject=$subject;
+    $mail->Body=$message;
+    $mail->isHTML(true);
+
+    $mail->send();
+    echo "Mail sent";
+    }
+
+}

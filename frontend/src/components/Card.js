efb5cdd087df66f67872124b2b9c642efcb8af64
loueysioua <louey.sioua@insat.ucar.tn>
2024-03-31 00:28:16 +0100

    updated frontend card component
    added animation for appearing report block

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,19 +2,36 @@ import OptionsButton from "./OptionsButton";
 import ReportButton from "./ReportButton";
 import LikeButton from "./LikeButton";
 import ShareButton from "./ShareButton";
-import { useState } from "react";
+import {useRef, useState} from "react";
+import { Transition } from 'react-transition-group';
 import { BsFillSendFill } from "react-icons/bs";
 
 export default function Card({ user, meme, profilePic, date }) {
   const [showReport, setShowReport] = useState(false);
+  const reportRef = useRef(null);
 
   const handleReportClick = () => {
     setShowReport(!showReport);
   };
 
+  const defaultStyle = {
+    transition: `height 500ms ease-in-out, opacity 500ms ease-in-out`,
+    height: '0px',
+    opacity: 0,
+    overflow: 'hidden'
+  };
+
+  const transitionStyles = {
+    entering: { height: '0px', opacity: 0 },
+    entered:  { height: reportRef.current ? `${reportRef.current.scrollHeight}px` : '0px', opacity: 1 },
+    exiting:  { height: reportRef.current ? `${reportRef.current.scrollHeight}px` : '0px', opacity: 1 },
+    exited:  { height: '0px', opacity: 0 },
+  };
+
+
   return (
     <div className="flex justify-center mt-10">
-      <div className="w-1/2 shadow-lg bg-gray-700 rounded-3xl relative">
+      <div className="w-1/2 shadow-lg bg-gray-700 rounded-3xl  relative ">
         <div className="flex items-center px-6 py-4">
           <img src={profilePic} alt="user" className="rounded-full" />
           <div className="flex flex-col ml-4">
@@ -25,21 +42,28 @@ export default function Card({ user, meme, profilePic, date }) {
           <OptionsButton memeId={meme?.id} />
         </div>
         <img src={meme} id={meme?.id} alt={meme?.title} className="w-full" />
-        <div className="px-6 py-4">
+        <div className={`px-6 py-4`}>
           <div className="flex items-center">
             <LikeButton />
             <ShareButton />
             <ReportButton onReportClick={handleReportClick} />
           </div>
-          <div className={`mt-4 flex flex-row justify-end space-x-5 items-start report-container ${showReport ? 'h-auto' : 'h-0'}`}>
-            <textarea
-              className="p-2 rounded w-1/2 h-20 border-black border-2 bg-gray-300"
-              placeholder="Write a report..."
-            />
-            <button className="mt-2 p-2 text-greens-200 text-2xl hover:text-greens-300 active:text-nightgreen">
-              <BsFillSendFill />
-            </button>
-          </div>
+          <Transition in={showReport} timeout={100}>
+            {state => (
+                <div ref={reportRef} style={{
+                  ...defaultStyle,
+                  ...transitionStyles[state]
+                }} className="mt-4 flex flex-row justify-end space-x-5 items-start report-container">
+                <textarea
+                    className="p-2 rounded w-1/2 h-20 border-black border-2 bg-gray-300"
+                    placeholder="Write a report..."
+                />
+                  <button className="mt-2 p-2 text-greens-200 text-2xl hover:text-greens-300 active:text-nightgreen">
+                    <BsFillSendFill />
+                  </button>
+                </div>
+            )}
+          </Transition>
         </div>
       </div>
     </div>

7064d0a7649c587d25eae34f63de0bf215285869
unknown <taki74ayadi@gmail.com>
2024-03-31 02:08:18 +0100

    configuration update

---
+++ backend/src/config/mailing.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Host' => 'smtp.gmail.com',
+    'SMTPAuth' => 'true',
+    'Username' => 'chellettakoua@gmail.com',
+    'Password' => 'wqvbhyxgjvrhwkzp',
+    'SMTPSecure'=>'tls',
+    'Port' => '587',
+];

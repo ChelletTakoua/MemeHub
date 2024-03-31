a43bf7bb59d267bb004611fc2806a69fd87f08b3
unknown <taki74ayadi@gmail.com>
2024-03-31 04:21:50 +0100

    getRequestParameters added

--- backend/src/Mailing/Mail.php
+++ backend/src/Mailing/Mail.php
@@ -8,28 +8,49 @@ require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
 class Mail{
-
+    /**
+     * Sends an email.
+     *
+     * @param string $to The recipient email address.
+     * @param string $subject The subject of the email.
+     * @param string $message The content of the email.
+     * @throws Exception If an error occurs while sending the email.
+     */
     static public function sendMail($to,$subject,$message){
-        $config = include __DIR__ . '/../config/mailing.php';
-        $mail=new PHPMailer(true);
+        try {
+            
+            $config = include __DIR__ . '/../config/mailing.php';
+            $mail=new PHPMailer(true);
 
-        $mail->isSMTP();
-        $mail->Host=$config['Host'];
-        $mail->SMTPAuth=$config['SMTPAuth'];
-        $mail->Username=$config['Username'];
-        $mail->Password=$config['Password'];
-        $mail->SMTPSecure=$config['SMTPSecure'];
-        $mail->Port=$config['Port'];
-        $mail->setFrom('chellettakoua@gmail.com');
+            $mail->isSMTP();
+            $mail->Host=$config['Host'];
+            $mail->SMTPAuth=$config['SMTPAuth'];
+            $mail->Username=$config['Username'];
+            $mail->Password=$config['Password'];
+            $mail->SMTPSecure=$config['SMTPSecure'];
+            $mail->Port=$config['Port'];
+            $mail->setFrom('chellettakoua@gmail.com');
 
-        $mail->addAddress($to);
-        $mail->Subject=$subject;
-        $mail->Body=$message;
-        $mail->isHTML(true);
+            $mail->addAddress($to);
+            $mail->Subject=$subject;
+            $mail->Body=$message;
+            $mail->isHTML(true);
 
-        $mail->send();
-        echo "Mail sent";
-    }
+            $mail->send();
+            echo "Email sent successfully";
+        } catch (Exception $e) {
+            echo "Email could not be sent.";
+        }  
+    }  
+    /**
+     * Sends an email with content loaded from a file, where placeholders are replaced with provided attributes.
+     *
+     * @param string $to The recipient email address.
+     * @param string $subject The subject of the email.
+     * @param string $path The path to the file to be attached.
+     * @param array $attributes Additional attributes to be replaced in the file content.
+     * @throws Exception If an error occurs while sending the email.
+     */
 
     static public function sendMailFile($to,$subject,$path,$attributes=[]){
         $file_content=file_get_contents( __DIR__.'/' .$path);
@@ -38,12 +59,25 @@ class Mail{
         }
         Self::sendMail($to,$subject,$file_content);
     }
+     /**
+     * Sends a welcome email to a newly created account, containing a verification link.
+     *
+     * @param string $to The recipient email address.
+     * @param string $username The username of the newly created account.
+     * @throws Exception If an error occurs while sending the email.
+     */
 
-    static public function account_created($to,$username){
+    static public function sendAccountCreatedMail($to,$username){
         Self::sendMailFile($to,"Welcome to Memehub !",'account-created.html',["username"=>$username]);
     }
-
-    static public function password_reset($to,$username){
+    /**
+     * Sends an email for password reset.
+     *
+     * @param string $to The recipient email address.
+     * @param string $username The username associated with the account.
+     * @throws Exception If an error occurs while sending the email.
+     */
+    static public function sendPasswordResetMail($to,$username){
         Self::sendMailFile($to,"password reset",'password-reset.html',["username"=>$username]);
     }
 
--- backend/src/Utils/RequestHandler.php
+++ backend/src/Utils/RequestHandler.php
@@ -13,4 +13,17 @@ class RequestHandler {
     {
         return json_decode(file_get_contents('php://input'), true) ?? [];
     }
+    /**
+     * Retrieves parameters from the current request.
+     *
+     * This function returns an associative array containing parameters passed to the current script,
+     * regardless of the HTTP request method used (GET, POST, etc.). It combines parameters from the
+     * $_GET, $_POST, and $_COOKIE superglobal arrays into a single array.
+     *
+     * @return array An associative array containing parameters from the current request.
+     */
+    public static function getRequestParameters() :array
+    {
+        return !empty($_REQUEST) ? $_REQUEST : [];
+    }
 }

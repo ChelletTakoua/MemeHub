8227a91c6bc58337e1c21de2588d5fc55c6da7d6
fouziMariem <mariem.elfouzi@insat.ucar.tn>
2024-04-02 01:38:29 +0100

    update debugging

--- backend/src/Debugging/debugging.php
+++ backend/src/Debugging/debugging.php
@@ -1,8 +1,17 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 global $router;
-//$router->printRoutes();
-?>
+$requests = $_SESSION['requests'];
+$index = 1;
 
+?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <title>Debugging</title>
+    <link href="../index.css" rel="stylesheet">
+</head>
+<body>
 <h1>Routes</h1>
 <table border='1'>
     <tr>
@@ -26,4 +35,34 @@ global $router;
 
 
 <!-- historique -->
+<h1>Historique</h1>
+<table border='1'>
+    <tr>
+        <th>Index</th>
+        <th>Method</th>
+        <th>Url</th>
+        <th>Date</th>
+        <th>Status code</th>
+        <th> </th>
+    </tr>
 
+    <?php foreach ($requests as $rq): ?>
+        
+            <tr>
+                <td><?= $index ++ ?></td>
+                <td><?= $rq['request']['method'] ?></td>
+                <td><?= $rq['request']['url'] ?></td>
+                <td><?= $rq['request']['timestamp'] ?></td>
+                <td><?= $rq['response']['status_code'] ?></td>
+                <td>
+                <?php
+// Assuming $index is already set to the desired value
+echo "<a href='/admin/requestDetails?index=" . $index . "'><button>Details</button></a>";
+?>
+                </td>
+            </tr>
+        
+    <?php endforeach; ?>
+</table>
+</body>
+</html>
